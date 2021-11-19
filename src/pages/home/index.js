59ed9c4c120b6bf9fdc39e208a2811bd505bff6d
estefaneluz <estefanecortes@gmail.com>
2021-11-19 11:16:31 -0300

    [code] componente Step criado e estilizado

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles.css';
 import Header from '../../components/Header';
+import Step from '../../components/Step';
 
 export default function Home() {
 	return (
 		<>
 			<Header/>
+			<Step />
 		</>
 	);
 }
