3d444e5bd6863ed7a47bc5afb5015c32592e2068
estefaneluz <estefanecortes@gmail.com>
2021-11-23 23:39:05 -0300

    [code] componente ProfileContent criado

---
+++ src/components/ProfileContent/ConsumeHeader.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { SelectedPrograms } from '../SelectedPrograms';
+import arrowUp from '../../assets/arrow-up.svg';
+import arrowDown from '../../assets/arrow-down.svg';
+
+import './style.css';
+
+export const ConsumeHeader = () => {
+	const [ clicked, setClicked ] = useState(false);
+
+	const handleClick = () => {
+		setClicked(!clicked);
+	};
+
+	return (
+		<div className="wrapper">
+			<div className="heading">
+				<p> Consumo </p>
+				<p className="selected" onClick={handleClick}>
+					Programas Selecionados
+					<img src={clicked ? arrowUp : arrowDown} alt="icone de seta" />
+				</p>
+			</div>
+			<div className="tag-wrapper">
+				<ul>
+					<li>
+						<div className="high" />
+						<p> Alto </p>
+					</li>
+					<li>
+						<div className="medium" />
+						<p> Médio </p>
+					</li>
+					<li>
+						<div className="low" />
+						<p> Baixo </p>
+					</li>
+				</ul>
+			</div>
+			{clicked && <SelectedPrograms />}
+		</div>
+	);
+};
---
+++ src/components/ProfileContent/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ConsumeHeader } from './ConsumeHeader'
+
+export const ProfileContent = () => {
+  return <>
+    <ConsumeHeader />
+  </>
+}
