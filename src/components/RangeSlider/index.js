eeec99478b4d2713c0d41531054b3a0cf0f75949
estefaneluz <estefanecortes@gmail.com>
2021-11-29 23:05:42 -0300

    [code] criando estado para o checkbox e imprimindo valor maximo e minimo na tela

--- src/components/RangeSlider/index.js
+++ src/components/RangeSlider/index.js
@@ -6,70 +6,70 @@ import DarkButton from '../DarkButton';
 import './style.css';
 
 function valuetext(value) {
-  return `R$ ${value}`;
+	return `R$ ${value}`;
 }
 
 const minDistance = 1;
 
-const marks = [
-  {
-    value: 1000,
-    label: 'R$ 1.000,00',
-  },
-  {
-    value: 20000,
-    label: 'R$ 20.000,000',
-  },
-];
-
 const currencyBRL = (value) => {
-  const formattedValue = value.toLocaleString(
-    'pt-BR', 
-    { style: 'currency', currency: 'BRL' }
-  );
+	const formattedValue = value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-    return formattedValue;
+	return formattedValue;
 };
 
 export const RangeSlider = () => {
-	const [price, setPrice] = React.useState([1500, 2000]);
+	const [price, setPrice ] = useState([ 1500, 3000 ]);
+	const [check, setCheck] = useState(false);  
+
+	const handleChecked = (e) => setCheck(e.target.checked);
 
 	const handlePrice = (event, newValue, activeThumb) => {
-    if (!Array.isArray(newValue)) {
-      return;
-    }
+		if (!Array.isArray(newValue)) {
+			return;
+		}
 
-    if (activeThumb === 0) {
-      setPrice([Math.min(newValue[0], price[1] - minDistance), price[1]]);
-    } else {
-      setPrice([price[0], Math.max(newValue[1], price[0] + minDistance)]);
-    }
-		
-  };
-
+		if (activeThumb === 0) {
+			setPrice([ Math.min(newValue[0], price[1] - minDistance), price[1] ]);
+		} else {
+			setPrice([ price[0], Math.max(newValue[1], price[0] + minDistance) ]);
+		}
+	};
 
 	return (
 		<div className="range-wrapper">
-			<Box sx={{ width: 470 }}>
-				<Slider
-					getAriaLabel={() => 'Minimum distance'}
-					value={price}
-					onChange={handlePrice}
-					valueLabelDisplay="auto"
-					getAriaValueText={valuetext}
-					valueLabelFormat={currencyBRL}
-					min={1000}
-					max={10000}
-					// marks={marks}
-					sx={{ color: 'white' }}
-					disableSwap
-				/>
-			</Box>
+			{!check && (
+				<>
+				<Box sx={{ width: 470, marginTop: 3 }}>
+					<Slider
+						getAriaLabel={() => 'Minimum distance'}
+						value={price}
+						onChange={handlePrice}
+						valueLabelDisplay="auto"
+						getAriaValueText={valuetext}
+						valueLabelFormat={currencyBRL}
+						min={1000}
+						max={10000}
+						sx={{ color: 'white' }}
+						disableSwap
+					/>
+				</Box>
+				<div className="row selected-price">
+					<p>
+						<span>Valor mínimo: </span> 
+						{price[0].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}
+					</p>
+					<p>
+						<span> Valor máximo: </span>
+						{price[1].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}
+					</p>
+				</div>
+				</>
+			)}
 			<div className="checkbox">
 				<label htmlFor="checkbox"> Sem valor estipulado </label>
-				<input name="checkbox" type="checkbox" />
+				<input checked={check} name="checkbox" type="checkbox" onChange={(e) => handleChecked(e)} />
 			</div>
-			<DarkButton label="Gerar Computador" icon={arrowRight}/>
+			<DarkButton label="Gerar Computador" icon={arrowRight} />
 		</div>
 	);
 };
