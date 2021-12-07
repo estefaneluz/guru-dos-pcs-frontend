55950a7f88529e0466a63a4ff16a02e4250a4743
estefaneluz <estefanecortes@gmail.com>
2021-12-06 21:19:38 -0300

    [code] função setStarIcon criada e organizando icones do feedback

--- src/components/Modal/ModalFeedback/index.js
+++ src/components/Modal/ModalFeedback/index.js
@@ -1,10 +1,22 @@
 import './styles.css'
-import React from 'react'
+import React, { useState } from 'react'
 import ModalBase from '../index'
 import starFilledIcon from '../../../assets/star-filled.svg'
 import starBorder from '../../../assets/star-border.svg'
 
 export default function ModalFeedback({handleModal, open}) {
+  const [howEasyToGenerate, setHowEasyToGenerate] = useState(-1);
+  const [serviceSatisfaction, setServiceSatisfaction] = useState(-1);
+  const [howEasyToUnderstand, setHowEasyToUnderstand] = useState(-1);
+
+  const setStarIcon = (state, value) => {
+    if(state >= value) {
+      return starFilledIcon
+    } else {
+      return starBorder
+    }
+  }
+
   return (
     <>
       <ModalBase handleModal={handleModal} open={open} className="column">
@@ -13,31 +25,76 @@ export default function ModalFeedback({handleModal, open}) {
           <div className="modal-feedback-question">
             <p>Foi fácil gerar um novo computador?</p>
             <div className="feedback-stars-container">
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 1 ponto." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 2 pontos." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 3 pontos."/>
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 4 pontos."/>
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 5 pontos." />
+              <img 
+                src={setStarIcon(howEasyToGenerate, 1)}
+                alt="icone de estrela para feedback. Vale 1 ponto."
+              />
+              <img 
+                src={setStarIcon(howEasyToGenerate, 2)}
+                alt="icone de estrela para feedback. Vale 2 pontos."
+              />
+              <img 
+                src={setStarIcon(howEasyToGenerate, 3)}
+                alt="icone de estrela para feedback. Vale 3 pontos."
+              />
+              <img 
+                src={setStarIcon(howEasyToGenerate, 4)}
+                alt="icone de estrela para feedback. Vale 4 pontos."
+              />
+              <img 
+                src={setStarIcon(howEasyToGenerate, 5)}
+                alt="icone de estrela para feedback. Vale 5 pontos."
+              />
             </div>
           </div>
           <div className="modal-feedback-question">
             <p>Qual nota você dá para o computador?</p>
             <div className="feedback-stars-container">
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 1 ponto." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 2 pontos." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 3 pontos." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 4 pontos." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 5 pontos." />
+              <img
+                src={setStarIcon(serviceSatisfaction, 1)}
+                alt="icone de estrela para feedback. Vale 1 ponto."
+              />
+              <img 
+                src={setStarIcon(serviceSatisfaction, 2)}
+                alt="icone de estrela para feedback. Vale 2 pontos."
+              />
+              <img 
+                src={setStarIcon(serviceSatisfaction, 3)}
+                alt="icone de estrela para feedback. Vale 3 pontos."
+              />
+              <img 
+                src={setStarIcon(serviceSatisfaction, 4)}
+                alt="icone de estrela para feedback. Vale 4 pontos."
+              />
+              <img 
+                src={setStarIcon(serviceSatisfaction, 5)}
+                alt="icone de estrela para feedback. Vale 5 pontos."
+              />
             </div>
           </div>
           <div className="modal-feedback-question">
             <p>Foi fácil entender as informações?</p>
             <div className="feedback-stars-container">
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 1 ponto." />
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 2 pontos."/>
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 3 pontos."/>
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 4 pontos."/>
-              <img src={starFilledIcon} alt="icone de estrela para feedback. Vale 5 pontos."/>
+              <img
+                src={setStarIcon(howEasyToUnderstand, 1)}
+                alt="icone de estrela para feedback. Vale 1 ponto."
+              />
+              <img 
+                src={setStarIcon(howEasyToUnderstand, 2)}
+                alt="icone de estrela para feedback. Vale 2 pontos."
+              />
+              <img
+                src={setStarIcon(howEasyToUnderstand, 3)}
+                alt="icone de estrela para feedback. Vale 3 pontos."
+              />
+              <img
+                src={setStarIcon(howEasyToUnderstand, 4)}
+                alt="icone de estrela para feedback. Vale 4 pontos."
+              />
+              <img
+                src={setStarIcon(howEasyToUnderstand, 5)}
+                alt="icone de estrela para feedback. Vale 5 pontos."
+              />
             </div>
           </div>
         </div>
