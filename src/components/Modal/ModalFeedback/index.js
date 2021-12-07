a262b0ea5f7029bdc32cc633df37ecfc19ba8336
estefaneluz <estefanecortes@gmail.com>
2021-12-06 22:33:57 -0300

    [code] requisição sendFeedback criada (ainda não funcionando, verificar backend)

--- src/components/Modal/ModalFeedback/index.js
+++ src/components/Modal/ModalFeedback/index.js
@@ -2,11 +2,59 @@ import './styles.css'
 import React, { useState } from 'react'
 import ModalBase from '../index'
 import FeedbackQuestion from './FeedbackQuestion'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function ModalFeedback({handleModal, open}) {
   const [howEasyToGenerate, setHowEasyToGenerate] = useState(0);
   const [serviceSatisfaction, setServiceSatisfaction] = useState(0);
   const [howEasyToUnderstand, setHowEasyToUnderstand] = useState(0);
+  const [message, setMessage] = useState('');
+
+  const [alertMessage, setAlertMessage] = useState({severity: '', message: ''});
+
+  const sendFeedback = async () => {
+    handleCloseAlert()
+    try {
+      console.log(howEasyToGenerate,
+        serviceSatisfaction,
+        howEasyToUnderstand,
+        message)
+      
+      const response = await fetch("http://localhost:5000/feedback", {
+        method: "POST",
+        body: JSON.stringify({
+          howEasyToGenerate,
+          serviceSatisfaction,
+          howEasyToUnderstand,
+          message
+        }),
+        headers: {
+          "Content-type": "application/json",
+        }, 
+      });
+
+      if(response.ok) {
+        setAlertMessage({
+          severity: 'success',
+          message: await response.json()
+        })
+      }
+    } catch {
+      setAlertMessage({
+        severity: 'error',
+        message: "Não foi possível enviar o feedback."
+      })
+    }
+  }
+
+  const handleCloseAlert = () => {
+    setAlertMessage({severity: '', message: ''});
+  };
 
   return (
     <>
@@ -29,12 +77,31 @@ export default function ModalFeedback({handleModal, open}) {
             setState={setHowEasyToUnderstand}
           />
         </div>
-        <textarea  placeholder="Campo livre para escrever alguma sugestão de melhoria." />
+        <textarea 
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Campo livre para escrever alguma sugestão de melhoria."
+        />
         <div className="row space-between">
           <button className="btn-transparent" onClick={handleModal}>Voltar</button>
-          <button className="btn-dark-green">Ir à loja</button>
+          <button className="btn-dark-green" onClick={sendFeedback}>Enviar</button>
         </div>
       </ModalBase>
+
+      <Snackbar
+        open={(alertMessage.severity === 'success' || alertMessage.severity === 'error')}
+        autoHideDuration={4000}
+        onClose={handleCloseAlert}
+      >
+          <Alert
+            severity={alertMessage.severity || 'success'}
+            sx={{ width: '100%' }}
+            onClose={handleCloseAlert}
+          >
+            {alertMessage.message}
+          </Alert>
+        </ Snackbar>
+      )
     </>
   );
 }
