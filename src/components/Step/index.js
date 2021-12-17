26038f054c3343dfb82ccb2e2ed2b4097ea03dd3
estefaneluz <estefanecortes@gmail.com>
2021-12-17 00:52:14 -0300

    [code] adicionando estilização do helptext

--- src/components/Step/index.js
+++ src/components/Step/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import './styles.css'
-import questionIcon from '../../assets/question-mark-icon.svg'
+import React from 'react';
+import './styles.css';
+import questionIcon from '../../assets/question-mark-icon.svg';
 
 export default function Step({ id, number, title, description, helpText, mt, mb }) {
-  return (
-    <div id={id} style={{ marginTop: mt, marginBottom: mb }} className="container-step">
-      <div className="step-number">{number}</div>
-      <h2>{title}</h2>
-      <p className="step-description">{description}</p>
-      {!!helpText && <img src={questionIcon} alt="icone de dúvida" />}
-    </div>
-  )
+	return (
+		<div id={id} style={{ marginTop: mt, marginBottom: mb }} className="container-step">
+			<div className="step-number">{number}</div>
+			<h2>{title}</h2>
+			<p className="step-description">{description}</p>
+			{!!helpText && (
+				<div className="tooltip">
+					<img src={questionIcon} alt="icone de dúvida" />
+					<span className="tooltiptext">{helpText}</span>
+				</div>
+			)}
+		</div>
+	);
 }
